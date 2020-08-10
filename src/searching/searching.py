820450b19ddc9858904755b5f75a3408e4c3c748
Joshua Sharp <sharp.josh@gmail.com>
2020-08-10 13:35:23 -0400

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,27 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for index, element in enumerate(arr):
+        if element == target:
+            return index
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    working_arr = arr[:]
+    float = 0
+    while len(working_arr) != 0:
+        mid = len(working_arr) // 2
+        value = working_arr[mid]
+        if target == value:
+            if float
+            return float - mid
+        if target > value:
+            working_arr = working_arr[mid:]
+        elif target < value:
+            working_arr = working_arr[0:mid]
+            float += len(arr) - len(working_arr)
+    return -1
 
-    # Your code here
-
-
-    return -1  # not found
+#
+# arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+# print(binary_search(arr1, 0) == 6)
